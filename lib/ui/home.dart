f9b0da46e9cd8c3ef6799e9eca39ae7b211a22d2
Panniat2212 <60070061@kmitl.ac.th>
2019-05-21 13:55:23 +0700

    first commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:mobilefinal/model/db.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return new HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int tmp = 0;
+  List tabs = [
+    Text("Home", style: TextStyle(fontSize: 25)),
+    Text("Notify", style: TextStyle(fontSize: 25)),
+    Text("Map", style: TextStyle(fontSize: 25)),
+    Text("Profile", style: TextStyle(fontSize: 25)),
+    Text("Setup", style: TextStyle(fontSize: 25))
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home",),centerTitle: true,
+      ),
+      body: Container(
+        child: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+          children: <Widget>[
+            ListTile(
+              title: Text('Hello ${CurrentUser.NAME}'),
+              subtitle: Text('this is my quote "${CurrentUser.QUOTE != null ? CurrentUser.QUOTE == '' ? 'ยังไม่มีการระบุข้อมูล' : CurrentUser.QUOTE : 'ยังไม่มีการระบุข้อมูล'}"'),
+            ),
+            RaisedButton(
+              child: Text("PROFILE SETUP"),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/profile');
+              },
+            ),
+            RaisedButton(
+              child: Text("MY FRIENDS"),
+              onPressed: () {
+                // Navigator.of(context).pushReplacementNamed('/friend');
+                Navigator.of(context).pushNamed('/friend');
+              },
+            ),
+            RaisedButton(
+              child: Text("SIGN OUT"),
+              onPressed: () {
+                CurrentUser.USERID = null;
+                CurrentUser.NAME = null;
+                CurrentUser.AGE = null;
+                CurrentUser.PASSWORD = null;
+                CurrentUser.QUOTE = null;
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
